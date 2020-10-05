d4b3eb040ed6ab150209fe27680191c990e68260
ericerodgers <ericerodgers@yahoo.com>
2020-10-05 15:18:58 -0400

    complete project

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//this debounce function controls the speed with which window.addEventListener fires
+//without using "wait" to slow the firing down to every 20ms like below
+//it would fire way more often and slow the whole page down
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function() {
@@ -11,4 +14,29 @@ function debounce(func, wait = 20, immediate = true) {
       timeout = setTimeout(later, wait);
       if (callNow) func.apply(context, args);
     };
-  }
+  };
+
+//select images
+const sliderImages = document.querySelectorAll('.slide-in');
+
+//function that runs every time the person scrolls
+function checkSlide() {
+    //loop over all images with a forEach
+    //for each slider image we want to fire the move when its element is 50% visible
+    sliderImages.forEach(sliderImage => {
+        const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+        //this handles when you scroll back UP:
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if(isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+    });
+}
+
+//listen for the scroll - and "debounce" the results before calling checkSlide function
+window.addEventListener('scroll', debounce(checkSlide));
